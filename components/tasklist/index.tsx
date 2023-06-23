802baf8937e1ad2ceee6459ab4c49d0a13d01b14
NoCaffeineNoCode <ivanbrocas01@gmail.com>
2023-06-23 17:59:32 -0300

    cambios menores

--- components/tasklist/index.tsx
+++ components/tasklist/index.tsx
@@ -43,7 +43,7 @@ export default function TasksList() {
     setTasks(res.tasks);
 
     response = await fetch(
-      `http://localhost:3000/api/tasks/focusTimes/?id=${idUser}`
+      `http://localhost:3000/api/tasks/focusTimes/${idUser}`
     );
     res = await response.json();
     setFocusTime(res.focusTime);
