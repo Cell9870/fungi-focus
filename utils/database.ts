3e72c70ce42533cc1d5b1065b1f37215e9b40afc
Cels03694 <cellschool2020@gmail.com>
2023-06-23 17:09:44 -0300

    andá loco por favor no seas malo

--- utils/database.ts
+++ utils/database.ts
@@ -6,7 +6,7 @@ export function connect(): Promise<mysql.Connection> {
   return mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
-    port: parseInt(process.env.DB_PORT || "3307"),
+    port: parseInt(process.env.DB_PORT || "3306"),
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
   });
